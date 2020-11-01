fc8af97a8432533b5db8e698b517b78b1a9bc22d
Azat Sharipov <sharipov.a@panoramik.ru>
2020-11-01 16:49:40 +0300

    test commit

---
+++ Assets/Scripts/Enums/AmmunitionType.cs
@@ -0,0 +1,14 @@
+﻿namespace FirstPersonShooter
+{
+    /// <summary>
+    /// Тип снарядов
+    /// </summary>
+    public enum AmmunitionType
+    {
+        None   = 0,
+        Rpg    = 2,
+        Bullet = 4,
+        ArmorPiercingBullet=8,
+        
+    }
+}
---
+++ Assets/Scripts/Model/AutomaticGun.cs
@@ -0,0 +1,24 @@
+﻿namespace FirstPersonShooter
+{
+    /// <summary>
+    /// Автоматическое оружие
+    /// </summary>
+    public sealed class AutomaticGun : Weapon
+    {
+        public override void Fire()
+        {
+            if (_isReady) 
+                return;
+            if (Clip.CountAmmunition <= 0) 
+                return;
+            var  obj = CacheObjectRepo.Instance.SpawnCachedObject(Ammunition.GetEnumMemberName(),
+                _barrel.position, _barrel.rotation,PoolingStratagy.EnqueueManuallyLater);
+            //CR: Роман, посмотри пж, можно ли так делать?
+            var tmpAmmunition = GetComponent<Ammunition>();
+            tmpAmmunition.AddForce(_barrel.forward*_force);
+            Clip.CountAmmunition--;
+            _isReady = false;
+            _timeRemaining.AddTimeRemaining();
+        }
+    }
+}
